659b140efdec3159481749a2dd80ce7a9f248494
Raul Ramirez <78058685+Raul-Sc@users.noreply.github.com>
2021-10-09 10:34:07 -0700

    INITIAL _SETUP

---
+++ Project2/Source/Project2/FighterCharacter.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+
+
+#include "FighterCharacter.generated.h"
+
+UCLASS()
+class PROJECT2_API AFighterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFighterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+		void MoveForward(float Value);
+
+	// Handles input for moving right and left.
+	UFUNCTION()
+		void MoveRight(float Value);
+};
