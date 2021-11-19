3df9f2056e60795a434bec3d967a04e459ab73f3
Raul Ramirez <78058685+Raul-Sc@users.noreply.github.com>
2021-11-19 10:52:16 -0800

    //script comments

--- Project02_BeS/Source/Project02_BeS/Project02_BeSGameModeBase.h
+++ Project02_BeS/Source/Project02_BeS/Project02_BeSGameModeBase.h
@@ -17,32 +17,37 @@ UCLASS()
 class PROJECT02_BES_API AProject02_BeSGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-
 		virtual void StartPlay() override;
 		 void print();
-
-
 private:
-
+	//Teleport to Locations
 	FVector locaA;
 	FVector locaB;
 	FVector locaC;
+	//inital world spawn 
 	FVector spawnOutLoc;
+	//initial facing spawn 
 	FRotator spawnRotation;
+
 	UPROPERTY(EditAnywhere, category = ThingsToSpawn)
 		TSubclassOf<class AEnemyFighterCharacter> enemyBP;
-
+	//list of enemies
 	TArray<AEnemyFighterCharacter*> horde;
-	void spawnIn(FVector location);
 
+	//more like teleport to 
+	void spawnIn(FVector location);
+	// teleport out of sight
 	void spawnOut(int index);
+	//spawn in a count of zombies every 3 seconds for n waves
 	UFUNCTION()
 	void spawnWave(int count, int waves);
 	FTimerHandle waveHandle;
+
+	//keep track of when next wave is up next 
 	int targetScore;
 	int enemiesAlive;
 
-	
+	//delegate of event when zombies die, they pass their index in the horde in order to spawn it out
 	UFUNCTION()
 	void updateScore(int index);
 public:
